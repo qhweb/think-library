ef6904ec152a20111fddf96f875b22dc0ae55d32
qhweb <qhweb@foxmail.com>
2020-08-25 09:16:33 +0800

    update

---
+++ src/Library.php
@@ -0,0 +1,293 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2020 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
+// +----------------------------------------------------------------------
+// | 官方网站: https://gitee.com/zoujingli/ThinkLibrary
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | gitee 代码仓库：https://gitee.com/zoujingli/ThinkLibrary
+// | github 代码仓库：https://github.com/zoujingli/ThinkLibrary
+// +----------------------------------------------------------------------
+
+namespace YYCms;
+
+use think\middleware\SessionInit;
+use think\Request;
+use think\Service;
+use think\helper\Str;
+use think\facade\Config;
+use think\facade\Lang;
+use think\facade\Cache;
+use think\facade\Event;
+use YYCms\middleware\Addons;
+
+/**
+ * 模块注册服务
+ * Class Library
+ * @package think\admin
+ */
+class Library extends Service
+{
+    protected $addons_path;
+
+    /**
+     * 启动服务
+     */
+    public function boot()
+    {
+        // 注册系统任务指令
+        $this->commands([
+            'YYCms\command\SendConfig',
+        ]);
+        //注册系统插件服务
+        $this->addonsBoot();
+    }
+
+    /**
+     * 初始化服务
+     */
+    public function register()
+    {
+        // 加载中文语言
+        $this->app->lang->load(__DIR__ . '/lang/zh-cn.php', 'zh-cn');
+        $this->app->lang->load(__DIR__ . '/lang/en-us.php', 'en-us');
+        // 输入变量默认过滤
+        $this->app->request->filter(['trim']);
+        // 判断访问模式，兼容 CLI 访问控制器
+        if ($this->app->request->isCli()) {
+            if (empty($_SERVER['REQUEST_URI']) && isset($_SERVER['argv'][1])) {
+                $this->app->request->setPathinfo($_SERVER['argv'][1]);
+            }
+        } else {
+            // 注册会话初始化中间键
+            if ($this->app->request->request('not_init_session', 0) == 0) {
+                $this->app->middleware->add(SessionInit::class);
+            }
+            // 注册访问处理中间键
+            $this->app->middleware->add(function (Request $request, \Closure $next) {
+                $header = [];
+                if (($origin = $request->header('origin', '*')) !== '*') {
+                    $header['Access-Control-Allow-Origin'] = $origin;
+                    $header['Access-Control-Allow-Methods'] = 'GET,POST,PATCH,PUT,DELETE';
+                    $header['Access-Control-Allow-Headers'] = 'Authorization,Content-Type,If-Match,If-Modified-Since,If-None-Match,If-Unmodified-Since,X-Requested-With';
+                    $header['Access-Control-Expose-Headers'] = 'User-Form-Token,User-Token,Token';
+                }
+            }, 'route');
+        }
+        $this->initAddons();
+    }
+
+    /**
+     * 插件初始化
+     */
+    private function initAddons()
+    {
+        $this->addons_path = $this->getAddonsPath();
+        // 自动载入插件
+        $this->autoload();
+        // 加载插件事件
+        $this->loadEvent();
+        // 加载插件系统服务
+        $this->loadService();
+        // 绑定插件容器
+        $this->app->bind('addons', Service::class);
+    }
+    /**
+     * 启动插件服务
+     */
+    private function addonsBoot()
+    {
+        $this->registerRoutes(function (Route $route) {
+            // 路由脚本
+            $execute = '\\YYCms\\addons\\Route::execute';
+
+            // 注册插件公共中间件
+            if (is_file($this->app->addons->getAddonsPath() . 'middleware.php')) {
+                $this->app->middleware->import(include $this->app->addons->getAddonsPath() . 'middleware.php', 'route');
+            }
+
+            // 注册控制器路由
+            $route->rule("addons/:addon/[:controller]/[:action]", $execute)->middleware(Addons::class);
+            // 自定义路由
+            $routes = (array) Config::get('addons.route', []);
+            foreach ($routes as $key => $val) {
+                if (!$val) {
+                    continue;
+                }
+                if (is_array($val)) {
+                    $domain = $val['domain'];
+                    $rules = [];
+                    foreach ($val['rule'] as $k => $rule) {
+                        [$addon, $controller, $action] = explode('/', $rule);
+                        $rules[$k] = [
+                            'addons'        => $addon,
+                            'controller'    => $controller,
+                            'action'        => $action,
+                            'indomain'      => 1,
+                        ];
+                    }
+                    $route->domain($domain, function () use ($rules, $route, $execute) {
+                        // 动态注册域名的路由规则
+                        foreach ($rules as $k => $rule) {
+                            $route->rule($k, $execute)
+                                ->name($k)
+                                ->completeMatch(true)
+                                ->append($rule);
+                        }
+                    });
+                } else {
+                    list($addon, $controller, $action) = explode('/', $val);
+                    $route->rule($key, $execute)
+                        ->name($key)
+                        ->completeMatch(true)
+                        ->append([
+                            'addons' => $addon,
+                            'controller' => $controller,
+                            'action' => $action
+                        ]);
+                }
+            }
+        });
+    }
+    /**
+     * 插件事件
+     */
+    private function loadEvent()
+    {
+        $hooks = $this->app->isDebug() ? [] : Cache::get('hooks', []);
+        if (empty($hooks)) {
+            $hooks = (array) Config::get('addons.hooks', []);
+            // 初始化钩子
+            foreach ($hooks as $key => $values) {
+                if (is_string($values)) {
+                    $values = explode(',', $values);
+                } else {
+                    $values = (array) $values;
+                }
+                $hooks[$key] = array_filter(array_map(function ($v) use ($key) {
+                    return [get_addons_class($v), $key];
+                }, $values));
+            }
+            Cache::set('hooks', $hooks);
+        }
+        //如果在插件中有定义 AddonsInit，则直接执行
+        if (isset($hooks['AddonsInit'])) {
+            foreach ($hooks['AddonsInit'] as $k => $v) {
+                Event::trigger('AddonsInit', $v);
+            }
+        }
+        Event::listenEvents($hooks);
+    }
+
+    /**
+     * 挂载插件服务
+     */
+    private function loadService()
+    {
+        $results = scandir($this->addons_path);
+        $bind = [];
+        foreach ($results as $name) {
+            if ($name === '.' or $name === '..') {
+                continue;
+            }
+            if (is_file($this->addons_path . $name)) {
+                continue;
+            }
+            $addonDir = $this->addons_path . $name . DIRECTORY_SEPARATOR;
+            if (!is_dir($addonDir)) {
+                continue;
+            }
+
+            if (!is_file($addonDir . ucfirst($name) . '.php')) {
+                continue;
+            }
+
+            $service_file = $addonDir . 'service.ini';
+            if (!is_file($service_file)) {
+                continue;
+            }
+            $info = parse_ini_file($service_file, true, INI_SCANNER_TYPED) ?: [];
+            $bind = array_merge($bind, $info);
+        }
+        $this->app->bind($bind);
+    }
+
+    /**
+     * 自动载入插件
+     * @return bool
+     */
+    private function autoload()
+    {
+        // 是否处理自动载入
+        if (!Config::get('addons.autoload', true)) {
+            return true;
+        }
+        $config = Config::get('addons');
+        // 读取插件目录及钩子列表
+        $base = get_class_methods("\\YYCms\\Addons");
+        // 读取插件目录中的php文件
+        foreach (glob($this->getAddonsPath() . '*/*.php') as $addons_file) {
+            // 格式化路径信息
+            $info = pathinfo($addons_file);
+            // 获取插件目录名
+            $name = pathinfo($info['dirname'], PATHINFO_FILENAME);
+            // 找到插件入口文件
+            if (strtolower($info['filename']) === 'plugin') {
+                // 读取出所有公共方法
+                $methods = (array)get_class_methods("\\addons\\" . $name . "\\" . $info['filename']);
+                // 跟插件基类方法做比对，得到差异结果
+                $hooks = array_diff($methods, $base);
+                // 循环将钩子方法写入配置中
+                foreach ($hooks as $hook) {
+                    if (!isset($config['hooks'][$hook])) {
+                        $config['hooks'][$hook] = [];
+                    }
+                    // 兼容手动配置项
+                    if (is_string($config['hooks'][$hook])) {
+                        $config['hooks'][$hook] = explode(',', $config['hooks'][$hook]);
+                    }
+                    if (!in_array($name, $config['hooks'][$hook])) {
+                        $config['hooks'][$hook][] = $name;
+                    }
+                }
+            }
+        }
+        Config::set($config, 'addons');
+    }
+
+    /**
+     * 获取 addons 路径
+     * @return string
+     */
+    public function getAddonsPath()
+    {
+        // 初始化插件目录
+        $addons_path = $this->app->getRootPath() . 'addons' . DIRECTORY_SEPARATOR;
+        // 如果插件目录不存在则创建
+        if (!is_dir($addons_path)) {
+            @mkdir($addons_path, 0755, true);
+        }
+
+        return $addons_path;
+    }
+
+    /**
+     * 获取插件的配置信息
+     * @param string $name
+     * @return array
+     */
+    public function getAddonsConfig()
+    {
+        $name = $this->app->request->addon;
+        $addon = get_addons_instance($name);
+        if (!$addon) {
+            return [];
+        }
+
+        return $addon->getConfig();
+    }
+}
